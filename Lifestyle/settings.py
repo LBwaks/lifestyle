f1010887af1619e6de0aee97e7ea087d95cb1047
LBwaks <obwakuvictor@gmail.com>
2023-04-17 11:06:44 +0300

    Deployment prep

--- Lifestyle/settings.py
+++ Lifestyle/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["lifestyle-production.up.railway.app"]
 # 'localhost',
 #   '127.0.0.1',
 #   '111.222.333.444',
