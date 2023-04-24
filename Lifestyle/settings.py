14fb62922376e7c51d3ce32f268e7afad10c8271
LBwaks <obwakuvictor@gmail.com>
2023-04-24 07:06:40 +0300

    Favicon

--- Lifestyle/settings.py
+++ Lifestyle/settings.py
@@ -50,15 +50,15 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://lifestyle-production.up.railway.app:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://lifestyle-production.up.railway.app:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 # 'localhost',
 #   '127.0.0.1',
 #   '111.222.333.444',
