ade88d6031a61e26d0080d0423e0cd518e798d9b
LBwaks <obwakuvictor@gmail.com>
2023-07-10 17:23:15 +0300

    aws setting

--- Lifestyle/settings.py
+++ Lifestyle/settings.py
@@ -679,8 +679,7 @@ AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 AWS_DEFAULT_ACL = "public-read"
-# AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-# AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+
 AWS_QUERYSTRING_AUTH = False
 AWS_QUERYSTRING_EXPIRE= 8
 # AWS_S3_OBJECT_PARAMETERS = {"Access-Control-Allow-Origin": "*"}
@@ -690,8 +689,6 @@ AWS_QUERYSTRING_EXPIRE= 8
 # AWS_CLOUDFRONT_KEY_ID =os.getenv("AWS_CLOUDFRONT_KEY_ID",None)
 # s3 static settings
 # AWS_LOCATION = "static"
-# STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
-# MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
